055db0e628e057e58ea84c162f91377216539149
Yoodeit <juno8147@gmail.com>
2022-12-11 15:50:48 +0900

    temp

--- skkutory/freeboard/models.py
+++ skkutory/freeboard/models.py
@@ -1,9 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
+#from django.contrib.auth.models import User
 
 
 # Create your models here.
-
 
 
 
@@ -12,7 +11,7 @@ class Freeboard(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, blank = True, null=True, related_name='post')
+    #owner = models.ForeignKey(User, on_delete=models.CASCADE, blank = True, null=True, related_name='post')
     #작성자인 owner라는 필드를 추가해보자.
     #django의 기본 User모델을 사용하였고, Foreignkey로 다대일 관계를 맺어주었다.
     #related_name을 통해 이후 역참조 시 Post를 찾을 수 있도록(나는 이걸 post로 해야 할까, freeboard로 해야 할까)
